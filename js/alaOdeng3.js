b8faad0bc30f0c1219b860c53e9c83d28d8b11de
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2021-02-26 08:28:09 +0800

    d

--- js/alaOdeng3.js
+++ js/alaOdeng3.js
@@ -1,15 +1,37 @@
-function numbers()
-{
-  let numbers = [];
-  for (let i = 1; i <= 100; i++){
-    numbers[i + 1] = i;
-  }
-  return numbers;
-}
+let actors = {
+	danial: 3,
+	george: 5,
+	sean: 9,
+	tim: 20,
+	joe: 21
+};
 
-// console.log(numbers());
+let minIndex = 0;
+let totalActors = Object.keys(actors);
+let count = Object.values(actors);
 
-numbers().forEach(function (number)
-{
-  console.log(number)
+//get min count in actors
+minIndex = count.indexOf(Math.min(...count));
+
+//print result
+
+console.log(Object.keys(actors)[minIndex]);
+
+var json = [
+	{
+		id: '1',
+		msg: 'hi',
+		tid: '2013-05-05 23:35',
+		fromWho: 'hello1@email.se'
+	},
+	{
+		id: '2',
+		msg: 'there',
+		tid: '2013-05-05 23:45',
+		fromWho: 'hello2@email.se'
+	}
+];
+
+json.forEach((item, index) => {
+	console.log(index);
 });
