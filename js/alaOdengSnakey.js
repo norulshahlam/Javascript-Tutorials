a3717c30497d773cd86ba1059abd911ab58ffd7f
norulshahlam <norulshahlam@gmail.com>
2021-03-09 01:11:59 +0800

    s

--- js/alaOdengSnakey.js
+++ js/alaOdengSnakey.js
@@ -14,45 +14,149 @@ $(() => {
       $table.append($cell);
     }
   }
-  let snakePos = { tr: 4, td: 5 };
-  let trValue = snakePos.tr;
-  let tdValue = snakePos.td;
-  let drawSnakePos = $("#tr" + trValue + "td" + tdValue);
+  const $start = $("<button>").text("Start").attr("id", "start");
+  $("body").append($start);
+
+  let snakePos = [
+    { tr: 4, td: 5 },
+    { tr: 3, td: 5 },
+    { tr: 2, td: 5 },
+  ];
+  let prevTrValue = 0;
+  let prevTdValue = 0;
+
+  let randPelletteTrVal = Math.floor(Math.random() * rowCell);
+  let randPelletteTdVal = Math.floor(Math.random() * rowCell);
+
+  let $score = $("div").attr("id", "score").text(0);
+  let currentScore = 0;
+
+  // auto move
+
+  // random pellette
+  const randomPellette = () => {
+    randPelletteTrVal = Math.floor(Math.random() * rowCell);
+    randPelletteTdVal = Math.floor(Math.random() * rowCell);
+
+    if()
+    let $drawPellete = $("#tr" + randPelletteTrVal + "td" + randPelletteTdVal);
+    $drawPellete.attr("class", "pellette");
+  };
+  randomPellette();
+
+  const updateScore = () => {
+    // add up to score value
+    currentScore++;
+    $score = $("div").attr("id", "score").text(currentScore);
+  };
+
+  const addSnake = () => {
+    snakePos.push({ tr: prevTrValue, td: prevTdValue });
+  };
 
   const drawSnake = () => {
-    console.log("draw snake: " + tdValue);
-    let drawSnakePos = $("#tr" + trValue + "td" + tdValue);
-    drawSnakePos.attr("id", "snake");
+    if (
+      $("#tr" + snakePos[0].tr + "td" + snakePos[0].td).attr("class") ===
+      "pellette"
+    ) {
+      addSnake();
+      randomPellette(); // make new pellette once its being hit
+      updateScore();
+    } else {
+      $("td").removeClass("snake");
+    }
+
+    // snakePos.unshift({ tr: prevTrValue, td: prevTdValue });
+    // snakePos.pop();
+    // console.log(snakePos);
+    for (let i = 0; i < snakePos.length; i++) {
+      let $drawSnakePos = $("#tr" + snakePos[i].tr + "td" + snakePos[i].td);
+      $drawSnakePos.attr("class", "snake");
+      // snakePos.unshift($drawSnakePos);
+      // snakePos.pop();
+    }
+
+    if (
+      snakePos[0].tr === -1 || // going outside of top border
+      snakePos[0].tr === rowCell || // going outside of bottom border
+      snakePos[0].td === -1 || // going outside of left border
+      snakePos[0].td === rowCell //going outside of right border
+    )
+      alert("GAME OVER!");
   };
 
-  const removeSnake = () => {
-    console.log("remove snake: " + tdValue);
-
-    $("#snake").attr("id", `tr${trValue}td${tdValue}`);
-  };
   drawSnake();
-
+  console.log(snakePos);
   document.addEventListener("keydown", (e) => {
+    console.log(snakePos);
     switch (e.keyCode) {
-      case 37: //left
-        removeSnake();
-        tdValue--;
+      case 37:
+        snakePos.unshift({ tr: snakePos[0].tr, td: snakePos[0].td - 1 });
+        snakePos.pop();
         drawSnake();
 
-      // case 39:
-      //   drawSnakePos = $("#tr" + (trValue + 1) + "td" + tdValue);
-      // case 40:
-      //   drawSnakePos = $("#tr" + trValue + "td" + (tdValue + 1));
-      // case 37:
-      //   drawSnakePos = $("#tr" + (trValue - 1) + "td" + tdValue);
+        break;
+      case 38:
+        snakePos.unshift({ tr: snakePos[0].tr - 1, td: snakePos[0].td });
+        snakePos.pop();
+        drawSnake();
+
+        break;
+      case 39:
+        snakePos.unshift({ tr: snakePos[0].tr, td: snakePos[0].td + 1 });
+        snakePos.pop();
+        drawSnake();
+        break;
+      case 40:
+        snakePos.unshift({ tr: snakePos[0].tr + 1, td: snakePos[0].td });
+        snakePos.pop();
+        drawSnake();
+
+        break;
     }
-    // console.log(e.keyCode);
+
+    e.preventDefault();
   });
-  // const snakeControl = () => {
-  // 	$("body").keydown(function () {
-  // 		console.log(e.keycode);
-  // 	});
+
+  // setInterval(() => {
+  // 	drawSnake();
+  // }, 1000);
+  //start button/reset
+  //snake auto moves
+  // const movSnake = () => {
+  //      snakePos[0].tr ++,
+  // }
+  //add snake tails
+  // const addSnake = () => {
+  // 	snakePos.push({ tr: prevTrValue, td: prevTdValue });
   // };
-  // snakeControl();
-  // snakeControl();
+  //snake hit its own body
 });
+// document.addEventListener("keydown", (e) => {
+//      switch (e.keyCode) {
+//           case 38:
+//                prevTdValue = snakePos[0].td;
+//                prevTrValue = snakePos[0].tr;
+//                console.log(prevTrValue, prevTdValue);
+//                snakePos[0].tr--;
+//                drawSnake();
+//                break;
+//           case 39:
+//                prevTdValue = snakePos[0].td;
+//                prevTrValue = snakePos[0].tr;
+//                snakePos[0].td++;
+//                drawSnake();
+//                break;
+//           case 40:
+//                prevTdValue = snakePos[0].td;
+//                prevTrValue = snakePos[0].tr;
+//                snakePos[0].tr++;
+//                drawSnake();
+//                break;
+//           case 37:
+//                prevTdValue = snakePos[0].td;
+//                prevTrValue = snakePos[0].tr;
+//                snakePos[0].td--;
+//                drawSnake();
+//                break;
+//      }
